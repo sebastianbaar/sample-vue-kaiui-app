a3d17485b4baafb4d8608009303bd38b12457bfc
Sebastian Baar <mail.sebastianbaar@gmail.com>
2020-05-02 14:16:10 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,127 @@
 <template>
   <kaiui-content>
-    <kaiui-header title="ToDo List" />
+    <kaiui-header title="Sample Vue KaiUI App" />
     <kaiui-tabs>
-      <kaiui-tab-item name="Item One" selected>
-        <kaiui-text
-          text="This is a standard Text element with a lot of Text in it. Hope you like it. Enjoy!"
-        />
-        <kaiui-input v-on:input="onInputChanged" label="Add a Task" placeholder="New Item" />
-        <kaiui-text v-bind:text="inputFieldText" />
-        <kaiui-separator title="Separator Title" />
-        <kaiui-input-multi label="Multi" placeholder="More Items" />
-        <kaiui-separator title="Separator Title" />
-        <kaiui-button icon="kai-icon-calendar" title="Show Dialog" v-on:softCenter="showDialog" />
+      <kaiui-tab-item name="Welcome" selected>
+        <kaiui-text text="Welcome to the Vue KaiUI sample App." />
+        <kaiui-text text="See all components live in action! Enjoy :-)" />
+      </kaiui-tab-item>
+
+      <kaiui-tab-item name="Buttons & Alerts" selected>
+        <kaiui-separator title="Buttons" />
         <kaiui-button
           v-bind:softkeys="softkeysPhone"
           v-on:softRight="phoneButtonSoftRightClicked"
           v-on:softCenter="phoneButtonSoftCenterClicked"
-          icon="kai-icon-phone"
+          icon="ion-ios-telephone"
           iconRight
-          title="Button Title"
+          title="Action Button"
         />
-        <kaiui-button title="Button Title" />
+
+        <kaiui-button title="Show Notice" v-on:softCenter="showANotice" />
+      </kaiui-tab-item>
+
+      <kaiui-tab-item name="Dialogs" selected>
+        <kaiui-button title="Dialog" v-on:softCenter="showDialog" />
+
+        <kaiui-button title="Value Selector Dialog" v-on:softCenter="showValueSelectorDialog" />
+
+        <kaiui-button icon="ion-android-calendar" title="ToDo: Date/Time Selector" />
+
+        <kaiui-dialog
+          title="Select"
+          v-model="shouldShowValueSelectorDialog"
+          v-on:softLeft="valueSelectorDialogLeftSelected"
+          v-on:softRight="valueSelectorDialogRightSelected"
+        >
+          <kaiui-checkbox
+            primaryText="Checkbox One"
+            secondaryText="Subtitle for Checkbox List Item Title"
+          />
+          <kaiui-checkbox
+            primaryText="Checkbox Two"
+            secondaryText="Subtitle for Checkbox List Item Title"
+          />
+          <kaiui-checkbox
+            primaryText="Checkbox Three"
+            secondaryText="Subtitle for Checkbox List Item Title"
+          />
+        </kaiui-dialog>
 
         <kaiui-dialog
           title="Congratulations"
-          text="You have successfully signed in. Good Job!"
           v-model="shouldShowDialog"
           v-on:softLeft="dialogLeftSelected"
           v-on:softRight="dialogRightSelected"
+        >
+          <kaiui-text text="You have successfully signed in. Good Job!" />
+        </kaiui-dialog>
+      </kaiui-tab-item>
+
+      <kaiui-tab-item name="Controls">
+        <kaiui-separator title="Radio button" />
+        <kaiui-radiogroup v-model="selectedInputValue">
+          <kaiui-radiobutton value="One" primaryText="Radio button 'One'" />
+          <kaiui-radiobutton
+            value="Two"
+            primaryText="Radio button 'Two'"
+            secondaryText="Subtitle for Radio button"
+          />
+          <kaiui-radiobutton
+            value="Three"
+            primaryText="Checkbox Item 'Three'"
+            secondaryText="Subtitle for Radio button"
+          />
+        </kaiui-radiogroup>
+        <kaiui-text v-bind:text="'> Selected: '+selectedInputValue" />
+
+        <kaiui-separator title="Checkbox" />
+        <kaiui-checkbox primaryText="Checkbox Item Title" />
+        <kaiui-checkbox
+          primaryText="Checkbox Item Title"
+          secondaryText="Subtitle for Checkbox List Item Title"
+        />
+        <kaiui-checkbox
+          primaryText="Checkbox Item Title"
+          secondaryText="Subtitle for Checkbox List Item Title"
         />
       </kaiui-tab-item>
 
-      <kaiui-tab-item name="Item Two">
+      <kaiui-tab-item name="List Items">
         <kaiui-list-item primaryText="List Item Title" />
-        <kaiui-list-item primaryText="List Item Title" iconLeft="kai-icon-download" />
+        <kaiui-list-item primaryText="List Item Title" iconLeft="ion-android-download" />
         <kaiui-list-item
           primaryText="List Item Title"
           secondaryText="Subtitle for List Item Title"
-          iconRight
         />
         <kaiui-list-item
           primaryText="List Item Title"
           secondaryText="Subtitle for List Item Title"
-          iconLeft="kai-icon-contacts"
+          iconLeft="ion-android-contacts"
           iconRight="none"
         />
-        <kaiui-list-item primaryText="List Item Title" iconLeft="kai-icon-favorite-on" />
+        <kaiui-list-item primaryText="List Item Title" iconLeft="ion-android-favorite" />
         <kaiui-list-item
           primaryText="List Item Title"
           secondaryText="Subtitle for List Item Title"
           tertiaryText="Very very long Tertiary Text for List Item Title"
-          iconLeft="kai-icon-calendar"
-          iconRight="kai-icon-favorite-on"
+          iconLeft="ion-android-calendar"
+          iconRight="ion-android-favorite"
         />
       </kaiui-tab-item>
 
-      <kaiui-tab-item name="Item Three">
-        <kaiui-button v-on:softCenter="toggleToastButtonSoftCenterClicked" title="Show Toast" />
-        <kaiui-checkbox
-          primaryText="Checkbox Item Title"
-          secondaryText="Subtitle for Checkbox List Item Title"
-          v-bind:isChecked="true"
-        />
-        <kaiui-checkbox primaryText="Checkbox Item Title" />
+      <kaiui-tab-item name="Inputs">
+        <kaiui-separator title="Inputs" />
+        <kaiui-input v-on:input="onInputChanged" label="Add a Task" placeholder="New Item" />
+        <kaiui-text v-bind:text="inputFieldText" />
+
+        <kaiui-separator title="Multiline Input" />
+        <kaiui-input-multi v-on:input="onMultiInputChanged" label="Title" placeholder="More Items" />
+        <kaiui-text v-bind:text="multiInputFieldText" />
       </kaiui-tab-item>
 
-      <kaiui-tab-item name="Item Four">
-        <kaiui-text text="Slider Fun..." />
-        <kaiui-separator title="Separator Title" />
+      <kaiui-tab-item name="Slider & Progress">
+        <kaiui-separator title="Slider" />
         <kaiui-slider
           title="Alarm"
           v-bind:startValue="sliderStartValue"
@@ -77,13 +131,9 @@
           v-bind:step="0.5"
         />
         <kaiui-text v-bind:text="sliderTextValue" />
-        <kaiui-checkbox primaryText="Checkbox Item Title" />
-      </kaiui-tab-item>
 
-      <kaiui-tab-item name="Item Five">
-        <kaiui-separator title="Separator Title" />
-        <kaiui-input label="Add a Task" placeholder="New Item" />
-        <kaiui-checkbox primaryText="Checkbox Item Title" />
+        <kaiui-separator title="Progress" />
+        <kaiui-text text="ToDo.." />
       </kaiui-tab-item>
     </kaiui-tabs>
   </kaiui-content>
@@ -96,9 +146,12 @@ export default {
   data: () => ({
     softkeysPhone: { left: "What's App", center: "Call", right: "SMS" },
     inputFieldText: "...",
+    multiInputFieldText: "...",
     sliderStartValue: 10,
     sliderTextValue: "Slider value is 10",
-    shouldShowDialog: false
+    shouldShowDialog: false,
+    shouldShowValueSelectorDialog: false,
+    selectedInputValue: "Two"
   }),
   methods: {
     toggleSoftkeys(input) {
@@ -119,14 +172,33 @@ export default {
     onInputChanged(newValue) {
       this.inputFieldText = newValue;
     },
+    onMultiInputChanged(newValue) {
+      this.multiInputFieldText = newValue;
+    },
+    showValueSelectorDialog() {
+      this.shouldShowValueSelectorDialog = !this.shouldShowValueSelectorDialog;
+    },
+    valueSelectorDialogRightSelected() {
+      this.showToast("Dialog 'OK' selected");
+    },
+    valueSelectorDialogLeftSelected() {
+      this.showToast("Dialog 'Cancel' selected");
+    },
+    showDialog() {
+      this.shouldShowDialog = !this.shouldShowDialog;
+    },
     dialogRightSelected() {
       this.showToast("Dialog 'OK' selected");
     },
     dialogLeftSelected() {
       this.showToast("Dialog 'Cancel' selected");
     },
-    showDialog() {
-      this.shouldShowDialog = !this.shouldShowDialog
+    showANotice() {
+      this.showNotice(
+        "ion-battery-charging",
+        "Battery Full",
+        "Battery is fully charged"
+      );
     }
   }
 };
